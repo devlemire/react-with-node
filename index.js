fa42e0d306f9f3a559ef47523d74e907fe030902
= <webdev.jameslemire@gmail.com>
2018-03-08 22:31:11 -0700

    JL master - Add data.

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const app = express()
 
 app.use( bodyParser.json() )
 
+let data = [
+   { id: 0, name: 'James' },
+   { id: 1, name: 'Murray' },
+   { id: 2, name: 'Richie' }
+]
+
 app.get('/api/read', ( req, res, next ) => {
    res.status(200).send('You have successfully read me')
 })
 
 app.post('/api/create', ( req, res, next ) => {
-   console.log(req.body)
    res.status(200).send(`${req.body.age}`)
 })
 
