e9004c21c955b3c43b4a0cca380cc977f0df1ea9
= <webdev.jameslemire@gmail.com>
2018-03-08 22:28:12 -0700

    JL master - Add post endpoint.

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const cors = require('cors')
 
 const app = express()
 
+app.use( bodyParser.json() )
+
 app.get('/api/read', ( req, res, next ) => {
    res.status(200).send('You have successfully read me')
+})
+
+app.post('/api/create', ( req, res, next ) => {
+   console.log(req.body)
+   res.status(200).send(`${req.body.age}`)
 })
 
 const port = 3001
